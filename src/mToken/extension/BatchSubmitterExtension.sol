8bcee28871b396f906a5ff41ed7d5c495f2c0556
team0 <team0.bctech@gmail.com>
2025-01-16 09:07:26 +0100

    extension submitter

--- src/mToken/extension/BatchSubmitterExtension.sol
+++ src/mToken/extension/BatchSubmitterExtension.sol
@@ -12,9 +12,8 @@ import {ZkVerifier} from "src/verifier/ZkVerifier.sol";
 contract BatchSubmitterExtension is ZkVerifier {
     error BatchSubmitterExtension_CallerNotAllowed();
     error BatchSubmitterExtension_JournalNotValid();
-    error BatchSubmitterExtension_LengthMismatch();
 
-    event BatchOutHereResult(address indexed mToken, bool success, bytes data);
+    event BatchOutHereFailed(bytes journal, bytes reason);
 
     /**
      * @notice The roles contract for access control
@@ -30,11 +29,13 @@ contract BatchSubmitterExtension is ZkVerifier {
     /**
      * @notice Execute multiple outHere operations in a single transaction
      * @param journalData The encoded journal data
+     * @param seal The seal data for verification
      * @param mTokens Array of mToken addresses
      * @param amounts Array of amounts for each operation
      */
     function batchOutHere(
         bytes calldata journalData,
+        bytes calldata seal,
         address[] calldata mTokens,
         uint256[] calldata amounts
     ) external {
@@ -48,30 +49,26 @@ contract BatchSubmitterExtension is ZkVerifier {
         // Decode journal data into array of bytes
         bytes[] memory journals = abi.decode(journalData, (bytes[]));
         
-        uint256 length = mTokens.length;
-        if (length != journals.length || length != amounts.length) {
-            revert BatchSubmitterExtension_LengthMismatch();
-        }
-
+        uint256 length = journals.length;
         for (uint256 i = 0; i < length;) {
             // Create single-element array and encode it
             bytes[] memory singleJournal = new bytes[](1);
             singleJournal[0] = journals[i];
             bytes memory encodedJournal = abi.encode(singleJournal);
 
+            // Create single-element array for amount
+            uint256[] memory singleAmount = new uint256[](1);
+            singleAmount[0] = amounts[i];
+
             try mTokenGateway(mTokens[i]).outHere(
                 encodedJournal,
                 "",
-                amounts
-            ) {
-                emit BatchOutHereResult(mTokens[i], true, "");
-            } catch (bytes memory reason) {
-                emit BatchOutHereResult(mTokens[i], false, reason);
+                singleAmount
+            ) {} catch (bytes memory reason) {
+                emit BatchOutHereFailed(journals[i], reason);
             }
 
-            unchecked {
-                ++i;
-            }
+            unchecked { ++i; }
         }
     }
 
