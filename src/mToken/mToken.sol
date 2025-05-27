c80bcd9b1fc2ab4dfc85cff39d68ceb960a8e5f0
Borna <94551425+Barnadrot@users.noreply.github.com>
2025-05-27 09:16:38 +0200

    Add license headers to Solidity files

--- src/mToken/mToken.sol
+++ src/mToken/mToken.sol
@@ -1,3 +1,18 @@
+// Copyright (c) 2025 Merge Layers Inc.
+//
+// This source code is licensed under the Business Source License 1.1
+// (the "License"); you may not use this file except in compliance with the
+// License. You may obtain a copy of the License at
+//
+//     https://github.com/malda-protocol/malda-lending/blob/main/LICENSE-BSL
+//
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// This file contains code derived from or inspired by Compound V2,
+// originally licensed under the BSD 3-Clause License. See LICENSE-COMPOUND-V2
+// for original license terms and attributions.
+
 // SPDX-License-Identifier: BSL-1.1
 pragma solidity =0.8.28;
 
@@ -346,7 +361,7 @@ abstract contract mToken is mTokenConfiguration, ReentrancyGuard {
     /**
      * @notice The sender liquidates the borrowers collateral.
      *  The collateral seized is transferred to the liquidator.
-     * @param borrower The liquidator address
+     * @param liquidator The liquidator address
      * @param borrower The borrower of this mToken to be liquidated
      * @param mTokenCollateral The market in which to seize collateral from the borrower
      * @param repayAmount The amount of the underlying borrowed asset to repay
--- src/oracles/MixedPriceOracleV4.sol
+++ src/oracles/MixedPriceOracleV4.sol
@@ -1,3 +1,14 @@
+// Copyright (c) 2025 Merge Layers Inc.
+//
+// This source code is licensed under the Business Source License 1.1
+// (the "License"); you may not use this file except in compliance with the
+// License. You may obtain a copy of the License at
+//
+//     https://github.com/malda-protocol/malda-lending/blob/main/LICENSE-BSL
+//
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 // SPDX-License-Identifier: BSL-1.1
 pragma solidity =0.8.28;
 
