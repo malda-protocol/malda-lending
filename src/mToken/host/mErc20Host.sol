24fa2411f3c445827207e1d3227c659d09eb85ea
cryptocoderkkk <grigore.cosmin@gmail.com>
2025-12-16 14:07:44 +0200

    reset market idea

--- src/mToken/host/mErc20Host.sol
+++ src/mToken/host/mErc20Host.sol
@@ -127,6 +127,14 @@ contract mErc20Host is mErc20Upgradable, ImErc20Host, ImTokenOperationTypes {
         _initHypernativeFirewall(_firewall, admin);
     }   
 
+    function resetMarket() external onlyAdmin {
+        totalBorrows = 0;
+        totalReserves = 0;
+        totalSupply = 0;
+        accrualBlockTimestamp = block.timestamp;
+        borrowIndex = mantissaOne;
+    }
+
     /**
      * @notice Updates an allowed chain status
      * @param _chainId the chain id
