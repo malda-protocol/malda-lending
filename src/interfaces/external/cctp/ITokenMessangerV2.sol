5788fa7254f413174e242664b24c311cd4428c0f
Satoshi <grigore.cosmin@gmail.com>
2026-01-28 15:03:26 +0200

    feat: added cctp bridge (#153)
    * feat: added cctp bridge
    
    * chore: nit picks
    
    * chore: PR fixes
    
    * chore: small event positioning
    
    * chore: added mainnet rpc url to github yml
    
    * chore: new everclear bridge for fee adapter v2
    
    * chore: small format
    
    * chore: PR fixes
    
    * updated TransferReleaseOwnership.s.sol
    
    * created UpgradeMarketFromMultisig.s.sol
    
    * chore: new tooling, consistent NatSpec, clarity comments (#156)
    
    * feat: introduced Makefile; resolved Slither issue
    
    * feat: add pre-commit hooks
    
    * chore: add linter
    
    * chore: fix linter issues in src
    
    * chore: fix linter issues in script
    
    * chore: fix linter issues in test
    
    * chore: added PR template file
    
    * chore: update GitHub Actions
    
    * docs: purging old docs before generating new ones
    
    * fix: merge issues
    
    * docs: update docs and introduce make target
    
    * chore: updated PR template
    
    * chore: cleanup scripts (#152)
    
    * fix: failing tests
    
    * fix: slither issues
    
    * chore: unify natspec from block comment to line comment
    
    * chore: format HypernativeFirewallProtected
    
    * chore: improve comments
    
    * add section comments
    
    * docs: update
    
    * docs: updated github pr template
    
    * chore: update Etherscan section of foundry.toml
    
    * chore: stop tracking docs/book
    
    * chore: fix doc backlinks
    
    * chore: pr comments
    
    * chore: remove new docs
    
    * fix: pr comments
    
    ---------
    
    Co-authored-by: Satoshi <grigore.cosmin@gmail.com>
    
    * Update ci.yml
    
    * chore: lint
    
    * chore: fmt
    
    * chore: lint
    
    * chore: lint
    
    * add missing tests
    
    ---------
    
    Co-authored-by: Igor Rončević <57319163+igorroncevic@users.noreply.github.com>
    Co-authored-by: igorroncevic <ironcevic98@gmail.com>

---
+++ src/interfaces/external/cctp/ITokenMessangerV2.sol
@@ -0,0 +1,54 @@
+// Copyright (c) 2025 Merge Layers Inc.
+//
+// This source code is licensed under the Business Source License 1.1
+// (the "License"); you may not use this file except in compliance with the
+// License. You may obtain a copy of the License at
+//
+//     https://github.com/malda-protocol/malda-lending/blob/main/LICENSE-BSL
+//
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// SPDX-License-Identifier: AGPL-3.0
+pragma solidity =0.8.28;
+
+/*
+ _____ _____ __    ____  _____
+|     |  _  |  |  |    \|  _  |
+| | | |     |  |__|  |  |     |
+|_|_|_|__|__|_____|____/|__|__|
+*/
+
+interface ITokenMessangerV2 {
+    /**
+     * @notice Deposits and burns tokens from sender to be minted on destination domain.
+     * Emits a `DepositForBurn` event.
+     * @dev reverts if:
+     * - `hookData` is zero-length
+     * - `burnToken` is not supported
+     * - `destinationDomain` has no TokenMessenger registered
+     * - transferFrom() reverts. For example, if sender's burnToken balance or approved allowance
+     * to this contract is less than `amount`.
+     * - burn() reverts. For example, if `amount` is 0.
+     * - maxFee is greater than or equal to `amount`.
+     * - MessageTransmitterV2#sendMessage reverts.
+     * @param amount amount of tokens to burn
+     * @param destinationDomain destination domain to receive message on
+     * @param mintRecipient address of mint recipient on destination domain, as bytes32
+     * @param burnToken token to burn `amount` of, on local domain
+     * @param destinationCaller authorized caller on the destination domain, as bytes32. If equal to bytes32(0),
+     * any address can broadcast the message.
+     * @param maxFee maximum fee to pay on the destination domain, specified in units of burnToken
+     * @param hookData hook data to append to burn message for interpretation on destination domain
+     */
+    function depositForBurnWithHook(
+        uint256 amount,
+        uint32 destinationDomain,
+        bytes32 mintRecipient,
+        address burnToken,
+        bytes32 destinationCaller,
+        uint256 maxFee,
+        uint32 minFinalityThreshold,
+        bytes calldata hookData
+    ) external;
+}
