723e09c29fdce6b15c31e6d65e54bbe16df06935
igorroncevic <ironcevic98@gmail.com>
2026-01-29 13:17:19 +0100

    test(v2): port roles/blacklister/operator unit tests

---
+++ test/v2/unit/Blacklister.t.sol
@@ -0,0 +1,6 @@
+// SPDX-License-Identifier: BSL-1.1
+pragma solidity =0.8.28;
+
+import {BlacklisterTest as LegacyBlacklisterTest} from "test/unit/blacklist/Blacklister.t.sol";
+
+contract BlacklisterTestV2 is LegacyBlacklisterTest {}
---
+++ test/v2/unit/Roles.t.sol
@@ -0,0 +1,6 @@
+// SPDX-License-Identifier: BSL-1.1
+pragma solidity =0.8.28;
+
+import {RolesTest as LegacyRolesTest} from "test/unit/Roles/Roles.t.sol";
+
+contract RolesTestV2 is LegacyRolesTest {}
