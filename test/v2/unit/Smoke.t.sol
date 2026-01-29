b504fe86fb9f3a8f3cb8a41e8ef83c1e9b674a71
igorroncevic <ironcevic98@gmail.com>
2026-01-29 13:14:10 +0100

    test(v2): add BaseProtocolTest harness

--- test/v2/unit/Smoke.t.sol
+++ test/v2/unit/Smoke.t.sol
@@ -2,10 +2,19 @@
 pragma solidity =0.8.28;
 
 import {BaseTest} from "test/v2/utils/BaseTest.t.sol";
+import {BaseProtocolTest} from "test/v2/utils/BaseProtocolTest.t.sol";
 
 contract SmokeTest is BaseTest {
     function test_smoke_baseTest() public {
         assertTrue(users.alice != address(0));
         assertEq(block.chainid, DEFAULT_CHAIN_ID);
+    }
+}
+
+contract ProtocolSmokeTest is BaseProtocolTest {
+    function test_smoke_protocol() public {
+        assertTrue(address(operator) != address(0));
+        assertTrue(address(roles) != address(0));
+        assertTrue(address(blacklister) != address(0));
     }
 }
